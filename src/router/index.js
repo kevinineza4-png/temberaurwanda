ec3d232d287199c277baf1dad983d6af821f2767
kevinineza4-png <kevinineza4@gmail.com>
2026-05-28 16:08:56 +0200

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,80 @@
-import {createrouter,createwebhistory} from "vue-router"
-import HomeView from "../views/HomeViews.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/home.vue'
+import About from '../views/About.vue'
+import services from '../views/services.vue'
+import contact from '../views/contact.vue'
+import video from '../views/video.vue'
+import Photos from '../views/photos.vue'
+import Kigali from '../views/kigali.vue'
+import northern from '../views/northern.vue'
+import southern from '../views/southern.vue'
+import Eastern from '../views/Eastern.vue'
+import western from '../views/western.vue'
 
-const routes =[
-    {
-        Path :"/",
-        name :"home",
-        Component:HomeView
-    },
-    
-];
-const router = createrouter(
-    {
-        history:createwebhistory(),
-        routes
-    });
-    export default router;
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+  {
+    path:'/services',
+    name:'services',
+    component:services,
+  },
+  {
+     path:'/contact',
+    name:'Contact',
+    component:contact,
+  },
+  {
+  path:'/video',
+    name:'video',
+    component:video,
+  },
+  {
+    path:'/photos',
+    name:'photos',
+    component:Photos, 
+  },
+  {
+  path:'/kigali',
+    name:'kigali',
+    component:Kigali,
+  },
+  {
+  path:'/northern',
+    name:'northern',
+    component:northern,
+  },
+  {
+     path:'/southern',
+    name:'southern',
+    component:southern,
+  },
+  {
+     path:'/Eastern',
+    name:'Eastern',
+    component:Eastern,
+  },
+  {
+    path:'/western',
+    name:'western',
+    component:western,
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,5 @@
+<script setup>
+</script>
+<template>
+    <section class="relative h-screen bg-center justify-between text-white" style="background-image:url('/src/assets/study.jpg')"></section>
+</template>
